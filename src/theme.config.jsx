9a2f3398cd7efec763f146a2cb565ab9b8f9031a
James Garner <j@jada.io>
2023-08-30 18:32:27 +0100

    Base config is now not "Nextra"

--- src/theme.config.jsx
+++ src/theme.config.jsx
@@ -6,5 +6,10 @@ export default {
   footer: {
     text: '© Sticky Connections Ltd'
   },
-  docsRepositoryBase: 'https://github.com/Stickyto/sticky-docs/tree/master/src'
+  docsRepositoryBase: 'https://github.com/Stickyto/sticky-docs/tree/master/src',
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s — Sticky'
+    }
+  }
 }
