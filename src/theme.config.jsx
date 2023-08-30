70592cf27dad645c7f663895a13ae5766f788a89
James Garner <j@jada.io>
2023-08-30 17:50:06 +0100

    Base config is now not "Nextra"

--- src/theme.config.jsx
+++ src/theme.config.jsx
@@ -2,7 +2,9 @@ export default {
   logo: <span>Sticky</span>,
   project: {
     link: 'https://github.com/Stickyto/sticky-docs',
-  }
+  },
+  footer: {
+    text: '© Sticky Connections Ltd'
+  },
+  docsRepositoryBase: 'https://github.com/Stickyto/sticky-docs/tree/master/src'
 }
-
-// https://nextra.site/docs/docs-theme/theme-configuration
