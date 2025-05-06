c2f2dca16ef4927272f9693c7b3e482751a0f50b
James <j@jada.io>
2025-05-06 14:23:50 +0100

    Document refunds

--- src/pages/_meta.js
+++ src/pages/_meta.js
@@ -1,10 +1,13 @@
 export default {
-  "index": "Home",
+  "index": "Index",
+  "embedded-payments": "Embedded payments",
+  "webhooks": "Webhooks",
+  "refunds": "Refunds",
   "pay-by-sticker": "Pay by sticker",
   "pay-by-link": "Pay by link",
-  "webhooks": "Webhooks",
   "tevalis--inbound-payments": "Sending payments to us",
   "embedded-pos": "Embedded POS",
+  "validate-a-session-code": "Validate a session code",
   "woocommerce": "WooCommerce",
   "kid-safety": {
     "display": "hidden"
