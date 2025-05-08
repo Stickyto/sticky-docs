6d8531337deb231d85e3dbe48376ea2ac2874484
James Garner <j@jada.io>
2025-05-08 17:51:02 +0100

    update

--- src/pages/_meta.js
+++ src/pages/_meta.js
@@ -3,7 +3,7 @@ export default {
   "embedded-payments": "Embedded payments",
   "webhooks": "Webhooks",
   "refunds": "Refunds",
-  "pay-by-sticker": "Pay by sticker",
+  "pay-by": "Pay by sticker or terminal",
   "pay-by-link": "Pay by link",
   "tevalis--inbound-payments": "Sending payments to us",
   "embedded-pos": "Embedded POS",
