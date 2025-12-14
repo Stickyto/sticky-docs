7898c9f8fcb161078bb3e574b64753ad1e1d4706
James <j@jada.io>
2025-12-14 21:20:28 +0000

    RM `build-preproduction.sh`

--- build-preproduction.sh
+++
@@ -1,6 +0,0 @@
-docker stop sticky-docs;
-docker rm sticky-docs;
-docker build -t sticky-docs .;
-
-docker tag sticky-docs eu.gcr.io/sticky-preproduction/sticky-docs:latest;
-docker push eu.gcr.io/sticky-preproduction/sticky-docs:latest;
